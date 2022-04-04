2b134fe7c0d3295f2815dc969eaafd679cbf1faf
drow724 <drow724@naver.com>
2022-04-04 10:09:24 +0900

    템플릿 메서드 패턴 - 예제2

--- src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -1,11 +1,15 @@
 package hello.advanced.trace.template;
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+
+import hello.advanced.trace.template.code.AbstractTemplate;
+import hello.advanced.trace.template.code.SubClassLogic1;
+import hello.advanced.trace.template.code.SubClassLogic2;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class TemplateMethodTest {
-	
+
 	@Test
 	void templateMethodV0() {
 		logic1();
@@ -21,7 +25,7 @@ public class TemplateMethodTest {
 		long resultTime = endTime - startTime;
 		log.info("resultTime={}", resultTime);
 	}
- 
+
 	private void logic2() {
 		long startTime = System.currentTimeMillis();
 		// 비즈니스 로직 실행
@@ -30,5 +34,16 @@ public class TemplateMethodTest {
 		long endTime = System.currentTimeMillis();
 		long resultTime = endTime - startTime;
 		log.info("resultTime={}", resultTime);
+	}
+
+	/**
+	 * 템플릿 메서드 패턴 적용
+	 */
+	@Test
+	void templateMethodV1() {
+		AbstractTemplate template1 = new SubClassLogic1();
+		template1.execute();
+		AbstractTemplate template2 = new SubClassLogic2();
+		template2.execute();
 	}
 }
