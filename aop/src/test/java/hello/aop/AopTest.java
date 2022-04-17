70cbc7ec3d515faee01874d31b17c080c8d075c2
Macintosh <drow724@naver.com>
2022-04-18 00:16:59 +0900

    스프링 AOP 구현4 - 포인트컷 참조

--- aop/src/test/java/hello/aop/AopTest.java
+++ aop/src/test/java/hello/aop/AopTest.java
@@ -10,25 +10,26 @@ import org.springframework.context.annotation.Import;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
-import hello.aop.order.aop.AspectV3;
+import hello.aop.order.aop.AspectV4Pointcut;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SpringBootTest
 //@Import(AspectV1.class)
 //@Import(AspectV2.class)
-@Import(AspectV3.class)
+//@Import(AspectV3.class)
+@Import(AspectV4Pointcut.class)
 public class AopTest {
-	
+
 	@Autowired
 	OrderService orderService;
-	
+
 	@Autowired
 	OrderRepository orderRepository;
 
 	@Test
 	void aopInfo() {
-		
+
 		log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService));
 		log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
 	}
